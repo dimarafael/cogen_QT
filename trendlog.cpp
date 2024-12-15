d3cb9f0f8b13cefbd5d2321b9a7bf2610ff55bbd
dimarafael <dimarafael@gmail.com>
2024-12-15 10:14:05 +0100

    trendlog bug fixes

--- trendlog.cpp
+++ trendlog.cpp
@@ -27,19 +27,30 @@ void Trendlog::startTrending(QAbstractSeries *series)
 
     qLineSeries->clear();
 
-    timer->start(1000);
-    connect(timer, &QTimer::timeout, this, &Trendlog::timerTimeout);
+    // timer->start(1000);
+    // connect(timer, &QTimer::timeout, this, &Trendlog::timerTimeout);
 
     m_startDateTime = QDateTime::currentDateTime();
     qLineSeries->append(QDateTime::currentDateTime().toMSecsSinceEpoch()-m_startDateTime.toMSecsSinceEpoch(),value());
+
+    if (!timer->isActive()) {
+        timer->start(1000);
+        connect(timer, &QTimer::timeout, this, &Trendlog::timerTimeout, Qt::UniqueConnection);
+    }
 }
 
 void Trendlog::stopTrending()
 {
     timer->stop();
+    qLineSeries = nullptr;
 }
 
 void Trendlog::timerTimeout()
 {
+    if (!qLineSeries) {
+        qWarning() << "qLineSeries is null. Skipping timerTimeout.";
+        return;
+    }
+
     qLineSeries->append(QDateTime::currentDateTime().toMSecsSinceEpoch()-m_startDateTime.toMSecsSinceEpoch(),value());
 }
